3adc9413b26c58850cbc1119a25ba95d662039f4
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2019-08-01 23:20:57 -0300

    final code

--- pulodogato.cpp
+++ pulodogato.cpp
@@ -5,24 +5,28 @@ using namespace std;
 
 int lajotas [MAX];
 int tamanho;
-int menorPulo(int index){
+int calculaPulos(int index){
 
-	if(index >= tamanho-1)//nesse caso, O
-		return 0;
-	if(lajotas[index] == 0)
-		return tamanho;
-	return 1 + min(menorPulo(index+1), menorPulo(index+2));
-}
+    if(index >= tamanho-1)
+	return 0;
+
+    if(lajotas[index] == 0)
+	return tamanho;
+
+    return 1 + min(calculaPulos(index+1), calculaPulos(index+2));
+}//end calculaPulos()
 
 int main(){
-	
-	scanf("%d", &tamanho);
-	for(int i = 0; i < tamanho; i++)
-		scanf("%d", &lajotas[i]);
-	int resultado = menorPulo(0);
-	if(resultado >= tamanho)
-		resultado = -1;
-	printf("%d", resultado);
 
-	return 0;
-}
+   scanf("%d", &tamanho);
+   for(int i = 0; i < tamanho; i++)
+   	 scanf("%d", &lajotas[i]);
+	 
+   int resultado = calculaPulos(0);
+   if(resultado >= tamanho)
+  	 resultado = -1;
+	 
+   printf("%d", resultado);
+
+   return 0;
+}//end main()
--- pulodogatodp.cpp
+++ pulodogatodp.cpp
@@ -1,33 +1,39 @@
-#include <iostream>
+#include <iostream> 
 #include <cstring> 
-
 using namespace std;
 #define MAX 10005
 
 int lajotas [MAX];
-int dp[MAX];
+int calculados[MAX];
 int tamanho;
-int menorPulo(int index){
 
-	if(dp[index]!=-1)
-		return dp[index];
-	if(index >= tamanho-1)//nesse caso, O
-		return 0;
-	if(lajotas[index] == 0)
-		return tamanho;
-	dp[index] = 1 + min(menorPulo(index+1), menorPulo(index+2));
-	return dp[index];
-}
+int calculaPulos(int index){
+
+    if(index >= tamanho-1)
+	return 0;
+
+    if(lajotas[index] == 0)
+	return tamanho;
+        
+    if(calculados[index] != -1)
+    	return calculados[index];
+	
+    calculados[index] = 1 + min(calculaPulos(index+1), calculaPulos(index+2));
+
+    return calculados[index];	    
+}//end calculaPulos()	
 
 int main(){
-	memset(dp, -1, sizeof(dp));
-	scanf("%d", &tamanho);
-	for(int i = 0; i < tamanho; i++)
-		scanf("%d", &lajotas[i]);
-	int resultado = menorPulo(0);
-	if(resultado >= tamanho)
-		resultado = -1;
-	printf("%d", resultado);
+   memset(calculados, -1, sizeof(calculados));
+   scanf("%d", &tamanho);
+   for(int i = 0; i < tamanho; i++)
+   	 scanf("%d", &lajotas[i]);
+	 
+   int resultado = calculaPulos(0);
+   if(resultado >= tamanho)
+  	 resultado = -1;
+	 
+   printf("%d", resultado);
 
-	return 0;
-}
+   return 0;
+}//end main()
